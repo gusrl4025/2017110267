04b40f26c90ffe30fe44eee05b00e41a6e81a6de
HyunKi Kim <gusrl4025@naver.com>
2020-04-17 11:29:59 +0900

    9단을 추가했습니다.

--- testProgram.cpp
+++ testProgram.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
-    for(int i=1; i<=8; i++){
+    for(int i=1; i<=9; i++){
         cout<<"[구구단 "<<i<<"단]"<<'\n';
         for(int j=1; j<=9; j++){
             cout<<i<<" x "<<j<<" = "<<i*j<<'\n';
